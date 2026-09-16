71a772aede362f7e7a023303a8898c0e8357d6b6
acer <acer@Z14-55N>
2026-09-16 14:59:37 +0530

    Add project files.

---
+++ assignment 2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Student
+{
+    // Field
+    private int age;
+
+    // Property
+    public string Name { get; set; }
+
+    public int Age
+    {
+        get { return age; }
+        set { age = value; }
+    }
+
+    // Constructor
+    public Student(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    // Method
+    public void Display()
+    {
+        Console.WriteLine("Name: " + Name);
+        Console.WriteLine("Age: " + Age);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating an object
+        Student student1 = new Student("Mohana", 25);
+
+        // Calling method
+        student1.Display();
+    }
+}
